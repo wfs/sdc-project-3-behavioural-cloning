4eba2c93402cfe6b07ed7fe902a6786741a56b18
andrew <hullpod.central@gmail.com>
2017-02-10 22:21:20 +1100

    King of the mountain! Track 2 success.

--- drive.py
+++ drive.py
@@ -1,28 +1,20 @@
 import argparse
 import base64
-import json
+from datetime import datetime
+import os
+import shutil
 
 import numpy as np
 import socketio
 import eventlet
 import eventlet.wsgi
-import time
 from PIL import Image
-from PIL import ImageOps
 from flask import Flask
-from flask import render_template
 from io import BytesIO
 
-from keras.models import model_from_json
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from keras.models import load_model
 
-# Fix error with Keras and TensorFlow
-import tensorflow as tf
-
-# Need to trim input image (like when did Training) for drive.py to work with the simulator, else car does not move.
 import model as NvidiaModel
-
-tf.python.control_flow_ops = tf
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -32,26 +24,35 @@ prev_image_array = None
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
-    # The current throttle of the car
-    throttle = data["throttle"]
-    # The current speed of the car
-    speed = data["speed"]
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    # Crop image to same dimensions as model was trained on, else simulator will not drive car on tracks.
-    cropped_image = NvidiaModel.Preprocess.crop_camera_image(np.asarray(image))
-    image_array = np.asarray(cropped_image)
-    transformed_image_array = image_array[None, :, :, :]
-    # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    # throttle = 0.2
-    throttle = .2 if float(speed) > 5 else 1.
-    print(steering_angle, throttle)
-    send_control(steering_angle, throttle)
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
+        # The current speed of the car
+        speed = data["speed"]
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        # Crop image to same dimensions as model was trained on, else simulator will not drive car on tracks.
+        cropped_image = NvidiaModel.Preprocess.crop_camera_image(np.asarray(image))
+        image_array = np.asarray(cropped_image)
+        # image_array = np.asarray(image)
+        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        # throttle = 0.2
+        throttle = 0.2 if float(speed) > 5 else 1.
+        # throttle = 0.1 if float(speed) > 5 else 1.  # Slowed, trying to complete track 2, top of mountain, tight turn. Failed!
+        print(steering_angle, throttle)
+        send_control(steering_angle, throttle)
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+    else:
+        # NOTE: DON'T EDIT THIS.
+        sio.emit('manual', data={}, skip_sid=True)
 
 
 @sio.on('connect')
@@ -61,23 +62,43 @@ def connect(sid, environ):
 
 
 def send_control(steering_angle, throttle):
-    sio.emit("steer", data={
-        'steering_angle': steering_angle.__str__(),
-        'throttle': throttle.__str__()
-    }, skip_sid=True)
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument('model', type=str,
-                        help='Path to model definition json. Preprocess weights should be on the same path.')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
     args = parser.parse_args()
-    with open(args.model, 'r') as jfile:
-        model = model_from_json(jfile.read())
 
-    model.compile("adam", "mse")
-    weights_file = args.model.replace('json', 'h5')
-    model.load_weights(weights_file)
+    model = load_model(args.model)
+
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
